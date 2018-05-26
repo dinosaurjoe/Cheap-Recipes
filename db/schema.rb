a633787658916c8b2276f8a6192e120c8e57ec54
Joe Schafer <joeschaferastula@gmail.com>
2018-05-26 11:09:32 -0500

    Added profile picture to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180526153932) do
+ActiveRecord::Schema.define(version: 20180526160208) do
 
   create_table "ingredients", force: :cascade do |t|
     t.integer "recipes_id"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 20180526153932) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "profile_picture"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
