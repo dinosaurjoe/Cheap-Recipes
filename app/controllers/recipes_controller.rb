e39b215dedb102a791196ecefc4477364ca7aa9a
Joe Schafer <joeschaferastula@gmail.com>
2018-05-28 12:08:51 -0500

    recipe controller create method and recipe params added

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,5 @@
 class RecipesController < ApplicationController
-  before_action :find_recipe, except: [:new]
+  before_action :find_recipe, except: [:new, :create]
 
   def show
     @creator = User.find(@recipe.user_id)
@@ -9,11 +9,26 @@ class RecipesController < ApplicationController
     @recipe = Recipe.new
   end
 
+  def create
+    @recipe = current_user.recipes.build(recipe_params)
+
+    if @recipe.save
+      # flash[:success] = "Your project was created. Now add some images."
+      redirect_to feed_path(@recipe)
+    else
+      flash[:alert] = @recipe.errors.full_messages.to_sentence
+      render :new
+    end
+  end
 
   private
 
   def find_recipe
     @recipe = Recipe.find(params[:id])
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :prep_time, :cooking_time, :short_description, :description)
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   mount_uploader :profile_picture, ImageUploader
 
+  has_many :recipes
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
