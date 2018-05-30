3fea442ae394d803aa0342182aced283919fff6a
Joe Schafer <joeschaferastula@gmail.com>
2018-05-30 09:43:41 -0500

    removed user id from uploads

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180527115750) do
+ActiveRecord::Schema.define(version: 20180530143215) do
 
   create_table "ingredients", force: :cascade do |t|
     t.integer "recipe_id"
@@ -39,12 +39,10 @@ ActiveRecord::Schema.define(version: 20180527115750) do
 
   create_table "uploads", force: :cascade do |t|
     t.string "image"
-    t.integer "user_id"
     t.integer "recipe_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["recipe_id"], name: "index_uploads_on_recipe_id"
-    t.index ["user_id"], name: "index_uploads_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
